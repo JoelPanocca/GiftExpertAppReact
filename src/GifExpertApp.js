3b8bdd368532cd2920abc5369c282e318826a399
Joel Panocca <panocca@hotmail.com>
2021-06-09 13:43:40 -0500

    Se agrega carpeta docs compilado

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -6,14 +6,13 @@ export const GifExpertApp = () => {
   const [categories, setCategories] = useState([
     "One Punch"
   ]);
-  
+
   return (
     <>
       <h2> GifExpert App</h2>
       
       <AddCategory setCategories = {setCategories}/>
       <hr />
-      {/* <button onClick={handleAdd}>Agregar </button> */}
       <ol>      
         {
             categories.map((cat) => {
